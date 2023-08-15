bd6164c33c326ef74b206792b95828fc18c3857b
TannerSPrice <110697995+TannerSPrice@users.noreply.github.com>
2023-08-15 14:37:28 -0500

    Update and rename alice_fell.pyproj to alice_fell.py

---
+++ alice_fell.py
@@ -0,0 +1,15 @@
+#defining alice fell down the rabit hole
+alice_fell = "Alice fell down, down, down into the rabbit hole."
+def into_the_rabbit_hole(): # define function of into the rabit hole
+     return alice_fell # returning the function
+
+print(alice_fell) #printing the defined variable
+
+
+
+# this is what a global variable looks like
+sentence = "Honey is sweet."
+def print_sentence():
+    print(sentence)
+   
+print_sentence()
