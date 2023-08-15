4c437759dbcf8717df78a913b1ca070162adf153
TannerSPrice <110697995+TannerSPrice@users.noreply.github.com>
2023-08-15 14:33:21 -0500

    Update and rename Parameters as local variables.pyproj to Parameters as local variables.py

---
+++ Parameters as local variables.py
@@ -0,0 +1,8 @@
+def my_function(value):
+  print(value)
+
+# pass the variable 8 into the function
+my_function(8)
+
+# Causes an error as 'value' has not been defiend and no longer exists
+#print(value)
