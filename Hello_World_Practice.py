02d0889518bfbe8854bcb73808c9a90dd95c6f66
TannerSPrice <110697995+TannerSPrice@users.noreply.github.com>
2023-08-15 14:31:55 -0500

    Update and rename Hello_World_Practice.pyproj to Hello_World_Practice.py

---
+++ Hello_World_Practice.py
@@ -0,0 +1,10 @@
+# simple Hello World POC
+
+
+part_of_message = " World"
+another_part_of_message = "Hello,"
+
+punctuation = "!"
+
+result = another_part_of_message + part_of_message + punctuation
+print(result)
