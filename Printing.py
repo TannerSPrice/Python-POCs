1fc01f80ee645dbb15633d62ba993ce918f74695
TannerSPrice <110697995+TannerSPrice@users.noreply.github.com>
2023-08-15 14:36:50 -0500

    Update and rename Printing.pyproj to Printing.py

---
+++ Printing.py
@@ -0,0 +1,6 @@
+# print an input by user and print the consecutive values in between.
+# example 123
+n = int(input())
+    
+for i in range(1, n+1):
+    print(i, end='') #end='' statement will take the numbers and print them consecutive horizontally versus vertically
