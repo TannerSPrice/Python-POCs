583df66861c5c187e899f2aa88e96d9443363b5a
TannerSPrice <110697995+TannerSPrice@users.noreply.github.com>
2023-08-15 14:30:29 -0500

    Update and rename Functions.pyproj to Functions.py

---
+++ Functions.py
@@ -0,0 +1,14 @@
+# defining a function called my_function() with paramater x
+def my_function(x):
+    return x + 1 
+
+# Invoking the function below
+
+# below will print out the function in two lines 
+
+print(my_function(2)) # here x becomes 2 + 1 which = 3
+# output should be 3 after invocation
+print(my_function(3 + 5)) # here we will run x which is 3 
+# and 3(which was x) moves back up the function of x + 1 and it now turns into 4 
+# then 3 ---> 4 then the computer will add 4 + 5 
+# which the output should be 9 
